26e7e30ebded28d80403249f73fd99da3ac9bcf8
Evgeny Tabatsky <jatx@DNS-debian>
2015-09-13 09:18:39 +1000

    JatxMusic repo created

---
+++ MusicReceiverAndroid/gen/jatx/musicreceiver/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jatx.musicreceiver.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MusicTransmitterAndroid/gen/jatx/musictransmitter/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package jatx.musictransmitter.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
