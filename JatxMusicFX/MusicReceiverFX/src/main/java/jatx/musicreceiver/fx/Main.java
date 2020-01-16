b98eabf35bd012eaba30067a306a506372bf000f
tabatsky <e.tabatsky@gmail.com>
2020-01-16 10:53:31 +1000

    just commit

---
+++ JatxMusicFX/MusicReceiverFX/src/main/java/jatx/musicreceiver/fx/Main.java
@@ -0,0 +1,7 @@
+package jatx.musicreceiver.fx;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFX.main(args);
+    }
+}
---
+++ JatxMusicFX/MusicTransmitterFX/src/main/java/jatx/musictransmitter/fx/Main.java
@@ -0,0 +1,7 @@
+package jatx.musictransmitter.fx;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFX.main(args);
+    }
+}
