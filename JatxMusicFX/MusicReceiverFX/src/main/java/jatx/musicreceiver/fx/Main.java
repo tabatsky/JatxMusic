0ab0b8fe9e40bb2c405603fd40634a3103017381
tabatsky <tabatsky@users.noreply.github.com>
2021-11-19 16:21:37 +1000

    JatxMusicFX updated

--- JatxMusicFX/MusicReceiverFX/src/main/java/jatx/musicreceiver/fx/Main.java
+++
@@ -1,7 +0,0 @@
-package jatx.musicreceiver.fx;
-
-public class Main {
-    public static void main(String[] args) {
-        MainFX.main(args);
-    }
-}
--- JatxMusicFX/MusicTransmitterFX/src/main/java/jatx/musictransmitter/fx/Main.java
+++
@@ -1,7 +0,0 @@
-package jatx.musictransmitter.fx;
-
-public class Main {
-    public static void main(String[] args) {
-        MainFX.main(args);
-    }
-}
